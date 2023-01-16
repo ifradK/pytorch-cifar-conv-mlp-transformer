4aa53ce5c8d269d44a33f5f5dbbf2708a6a84106
ifradK <55544024+ifradK@users.noreply.github.com>
2023-01-17 00:33:08 +0600

    Update train_tinyimagenet.py

--- train_tinyimagenet.py
+++ train_tinyimagenet.py
@@ -197,12 +197,12 @@ if aug:
 # trainset = torchvision.datasets.ImageNet(root='./data', train=True, download=True, transform=transform_train)
 # trainloader = torch.utils.data.DataLoader(trainset, batch_size=bs, shuffle=True, num_workers=8)
 
-trainloader = load_train_data(384, 9, args.bs)
+trainloader = load_train_data(args.size, 9, args.bs)
 
 # testset = torchvision.datasets.ImageNet(root='./data', train=False, download=True, transform=transform_test)
 # testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=8)
 
-testloader = load_val_data(384, args.bs)
+testloader = load_val_data(args.size, args.bs)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
